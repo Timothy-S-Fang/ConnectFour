38ea0b3749496bf3436fb060d275355b2735257f
Timothy Fang <timothyfang@student-232-8.eduroam.uu.se>
2023-09-20 14:39:25 +0200

    test push

--- connectfour.py
+++ connectfour.py
@@ -23,3 +23,34 @@ def game_result():
     # 0 : lose, 1: win, 2: draw
     return
 
+def get_open_row():
+    # return the next open row given a column from top down
+    return
+
+game_board = create_board()
+game_over = False
+turn = 0
+
+while not game_over:
+
+    # Player one turn
+    if turn == 0:
+        col = int(input("Player 1 Choose your move from (0-6)"))
+        if valid_move():
+            row = get_open_row()
+            player_turn(...)
+
+    # Player two turn
+    else:
+        col = int(input("Player 2 Choose your move from (0-6)"))
+        if valid_move():
+            row = get_open_row()
+            player_turn(...)
+
+            if ():
+                print("Congratulations, Player 2 wins!")
+                game_over = True
+    
+    print_board()
+    turn += 1
+    turn = turn % 2
