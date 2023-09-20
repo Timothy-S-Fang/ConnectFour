48cf38693d09a4484da7ff3543fe93baf2312111
Timothy Fang <timothyfang@student-232-8.eduroam.uu.se>
2023-09-20 14:14:07 +0200

    initial function definitions

--- connectfour.py
+++ connectfour.py
@@ -1,2 +1,25 @@
 
-print('hello world')
+def create_board():
+    # Initialize the board
+    return
+
+def player_turn():
+    # make a move
+    return
+
+def valid_move():
+    # returns boolean on whether given spot is available
+    return
+
+def print_board():
+    # print current game state
+    return
+
+def winning_move():
+    # check if there is a winning move
+    return
+
+def game_result():
+    # 0 : lose, 1: win, 2: draw
+    return
+
