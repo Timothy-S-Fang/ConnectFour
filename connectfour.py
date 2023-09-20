71b0f990c9e109fe240c538871e754be7796e1ed
Sarah Sindone <sindone.sarah@gmail.com>
2023-09-20 14:54:29 +0200

    updated create_board

--- connectfour.py
+++ connectfour.py
@@ -1,7 +1,17 @@
+import numpy
+
+board_size = (6,7) #size of the columns, rows
+cell_empty = 0 #empty cell on board
+player_x = 1 #player x piece on board
+player_y = 2 #player y piece on board
+
+#define game_board using 2D numpy array
+game_board = numpy.zeros(board_size, dtype= int)
 
 def create_board():
-    # Initialize the board
-    return
+    # Initialize the board by uing the 2D numpy array
+    global game_board
+    game_board = numpy.zeros(board_size, dtype= int)
 
 def player_turn():
     # make a move
