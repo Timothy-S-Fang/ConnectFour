0a26cf7bf2f0096a280e59d2bbf1f4556ca3809c
Liv Martin Björkdahl <livmartinbjorkdahl@student-226-164.eduroam.uu.se>
2023-09-20 14:42:58 +0200

    valid move, player turn, print board

--- connectfour.py
+++ connectfour.py
@@ -3,21 +3,17 @@ def create_board():
     # Initialize the board
     return
 
-def player_turn():
+def player_turn(board,row, col, piece):
     # make a move
-    return
+    board[row][col] = piece
 
-def valid_move():
+def valid_move(board, col):
     # returns boolean on whether given spot is available
-    return
+    return board[-1][col] == 0
 
 def print_board():
     # print current game state
-    return
-
-def winning_move():
-    # check if there is a winning move
-    return
+    print(np.flip(board))
 
 def game_result():
     # 0 : lose, 1: win, 2: draw
