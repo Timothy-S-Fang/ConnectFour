98682b4968a2deb85d70b8143e64041a8785f9da
Timothy Fang <timothyfang@student-219-220.eduroam.uu.se>
2023-09-25 14:49:30 +0200

    fixed game_progress check

--- connectfour.py
+++ connectfour.py
@@ -23,6 +23,8 @@ def player_turn(board, row, col, piece):
 
 def valid_move(board, col):
     # returns boolean on whether given spot is available
+    if col > 6 or col < 0:
+        return False
     return board[-1][col] == 0
 
 def print_board():
@@ -31,48 +33,48 @@ def print_board():
     return
 
 def game_progress():
-    # check if there is a winning move
+    # check if there is a winning move and return value of game_over
 
     # Horizontal win
     for col in range(COL_SIZE - 3):
         for row in range(ROW_SIZE):
             if game_board[row][col] == cell_empty:
-                return 
+                break 
             player = game_board[row][col]
             if game_board[row][col + 1] == player and game_board[row][col + 2] == player and game_board[row][col + 3] == player:
-                print("Congratulations! Player " + str(player) + "has won the game on a horizontal connection!")
-                game_over = True
+                print("Congratulations! Player " + str(player) + " has won the game on a horizontal connection!")
+                return True
     
     # Vertical win
     for col in range(COL_SIZE):
         for row in range(ROW_SIZE - 3):
             if game_board[row][col] == cell_empty:
-                return 
+                break  
             player = game_board[row][col]
-            if game_board[row][col + 1] == player and game_board[row + 2][col] == player and game_board[row + 3][col] == player:
-                print("Congratulations! Player " + str(player) + "has won the game on a horizontal connection!")
-                game_over = True
+            if game_board[row + 1][col] == player and game_board[row + 2][col] == player and game_board[row + 3][col] == player:
+                print("Congratulations! Player " + str(player) + " has won the game on a vertical connection!")
+                return True
     
     # Positively sloped angles
     for col in range(COL_SIZE - 3):
         for row in range(ROW_SIZE - 3):
             if game_board[row][col] == cell_empty:
-                return 
+                break  
             player = game_board[row][col]
             if game_board[row + 1][col + 1] == player and game_board[row + 2][col + 2] == player and game_board[row + 3][col + 3] == player:
-                print("Congratulations! Player " + str(player) + "has won the game on a diagonal connection!")
-                game_over = True
+                print("Congratulations! Player " + str(player) + " has won the game on a diagonal connection!")
+                return True
 
     # Negatively sloped angles
     for col in range(COL_SIZE - 3):
         for row in range(3, ROW_SIZE):
             if game_board[row][col] == cell_empty:
-                return 
+                break  
             player = game_board[row][col]
             if game_board[row - 1][col + 1] == player and game_board[row - 2][col + 2] == player and game_board[row - 3][col + 3] == player:
-                print("Congratulations! Player " + str(player) + "has won the game on a diagonal connection!")
-                game_over = True
-    return
+                print("Congratulations! Player " + str(player) + " has won the game on a diagonal connection!")
+                return True
+    return False
 
 def get_open_row(c):
     # return the next open row given a column from top down
@@ -89,9 +91,10 @@ while not game_over:
         col = int(input("Player 1 Choose your move from (0-6)"))
         if valid_move(game_board, col):
             row = get_open_row(col)
-            print(row)
             player_turn(game_board, row, col, player_x)
-            game_progress()
+            game_over = game_progress()
+        else:
+            print('invalid move')
 
     # Player two turn
     else:
@@ -99,6 +102,9 @@ while not game_over:
         if valid_move(game_board, col):
             row = get_open_row(col)
             player_turn(game_board, row, col, player_y) 
+            game_over = game_progress()
+        else:
+            print('invalid move')
 
     
     turn += 1
