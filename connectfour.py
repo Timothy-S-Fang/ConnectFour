ac1eac35148b659a6a87f6d58eef009a3d4b209f
Sarah Sindone <sindone.sarah@gmail.com>
2023-10-12 14:45:42 +0200

    large changes to connect four and minimax files

--- connectfour.py
+++ connectfour.py
@@ -1,69 +1,75 @@
 import numpy as np
-import random
 
 ROW_SIZE = 7
 COL_SIZE = 6
-board_size = (COL_SIZE,ROW_SIZE) #size of the columns, rows
-cell_empty = 0 #empty cell on board
-player_x = 1 #player x piece on board
-player_y = 2 #player y piece on board
+board_size = (COL_SIZE, ROW_SIZE)
+cell_empty = 0
+player_x = 1
+player_y = 2
 game_over = False
 turn = 0
 
-#define game_board using 2D np array
-game_board = np.zeros(board_size, dtype= int)
+game_board = np.zeros(board_size, dtype=int)
 
 def create_board():
-    # Initialize the board by uing the 2D np array
     global game_board
-    game_board = np.zeros(board_size, dtype= int)
+    game_board = np.zeros(board_size, dtype=int)
 
 def player_turn(board, row, col, piece):
-    # make a move
     board[row][col] = piece
 
 def valid_move(board, col):
-    # returns boolean on whether given spot is available
-    if col > 6 or col < 0:
-        return False
-    return board[-1][col] == 0
-    #TODO: avoid breaking if number outside grid of the board is picked
+    if 0 <= col < COL_SIZE:
+        return board[-1][col] == 0
+    return False
 
 def print_board():
-    # print current game state
     print(np.flip(game_board))
     return
 
 def game_progress():
-    # check if there is a winning move and return value of game_over
-
-    # Horizontal win
     for col in range(COL_SIZE - 3):
         for row in range(ROW_SIZE):
             if game_board[row][col] == cell_empty:
-                break 
+                continue
             player = game_board[row][col]
-            if game_board[row][col + 1] == player and game_board[row][col + 2] == player and game_board[row][col + 3] == player:
+            if (
+                col + 3 < COL_SIZE
+                and game_board[row][col + 1] == player
+                and game_board[row][col + 2] == player
+                and game_board[row][col + 3] == player
+            ):
                 print("Congratulations! Player " + str(player) + " has won the game on a horizontal connection!")
                 return True
-    
+
     # Vertical win
     for col in range(COL_SIZE):
         for row in range(ROW_SIZE - 3):
             if game_board[row][col] == cell_empty:
-                break  
+                continue
             player = game_board[row][col]
-            if game_board[row + 1][col] == player and game_board[row + 2][col] == player and game_board[row + 3][col] == player:
+            if (
+                row + 3 < ROW_SIZE
+                and game_board[row + 1][col] == player
+                and game_board[row + 2][col] == player
+                and game_board[row + 3][col] == player
+            ):
                 print("Congratulations! Player " + str(player) + " has won the game on a vertical connection!")
                 return True
-    
+
     # Positively sloped angles
     for col in range(COL_SIZE - 3):
         for row in range(ROW_SIZE - 3):
             if game_board[row][col] == cell_empty:
-                break  
+                continue
             player = game_board[row][col]
-            if game_board[row + 1][col + 1] == player and game_board[row + 2][col + 2] == player and game_board[row + 3][col + 3] == player:
+            if (
+                row + 3 < ROW_SIZE
+                and col + 3 < COL_SIZE
+                and game_board[row + 1][col + 1] == player
+                and game_board[row + 2][col + 2] == player
+                and game_board[row + 3][col + 3] == player
+            ):
                 print("Congratulations! Player " + str(player) + " has won the game on a diagonal connection!")
                 return True
 
@@ -71,41 +77,56 @@ def game_progress():
     for col in range(COL_SIZE - 3):
         for row in range(3, ROW_SIZE):
             if game_board[row][col] == cell_empty:
-                break  
+                continue
             player = game_board[row][col]
-            if game_board[row - 1][col + 1] == player and game_board[row - 2][col + 2] == player and game_board[row - 3][col + 3] == player:
+            if (
+                row - 3 >= 0
+                and col + 3 < COL_SIZE
+                and game_board[row - 1][col + 1] == player
+                and game_board[row - 2][col + 2] == player
+                and game_board[row - 3][col + 3] == player
+            ):
                 print("Congratulations! Player " + str(player) + " has won the game on a diagonal connection!")
                 return True
+
     return False
 
-def get_open_row(c):
-    # return the next open row given a column from top down
-    for row in range(ROW_SIZE):
-        if game_board[row][c] == cell_empty:
-            return row
+def get_open_row(col):
+    if 0 <= col < COL_SIZE:
+        for row in range(ROW_SIZE - 1, -1, -1):
+            if game_board[row][col] == cell_empty:
+                return row
+    return None
+                                                       
 
-    return
 
 while not game_over:
     print_board()
 
-    # Player one turn
+    # Player one turn (User)
     if turn == 0:
         col = int(input("Player 1 Choose your move from (0-6)"))
-        if valid_move(game_board, col):
-            row = get_open_row(col)
-            player_turn(game_board, row, col, player_x)
-            game_over = game_progress()
+        if 0 <= col < COL_SIZE:  # Check if col is within bounds
+            row = get_open_row(col)  
+            if row is not None:
+                player_turn(game_board, row, col, player_x)
+                game_over = game_progress()
+            else:
+                print('Invalid move')
         else:
-            print('invalid move')
+            print('Column index out of bounds. Please choose a column between 0 and', COL_SIZE - 1)
 
-    # Player two turn
+    # Player two turn (AI)
     else:
-        col = int(input("Player 2 Choose your move from (0-6)"))
-        if valid_move(game_board, col):
-            row = get_open_row(col)
-            player_turn(game_board, row, col, player_y) 
+        print("Player 2 (AI) is thinking...")
+        col = ai_move(game_board)  # Call the AI function to make a move
+        if 0 <= col < COL_SIZE:  # Check if col is within bounds
+            row = get_open_row(col)  # Uncomment and place this line here to assign a valid row
+            if row is not None:
+                player_turn(game_board, row, col, player_y)
+                game_over = game_progress()
+            else:
+                print('AI chose an out-of-bounds column. This is an issue in your AI logic.')
 
-    
     turn += 1
     turn = turn % 2
--- minimax.py
+++ minimax.py
@@ -1,112 +1,50 @@
 import numpy as np
+from connectfour import ROW_SIZE, COL_SIZE, AI_PLAYER, HUMAN_PLAYER, game_board, valid_move, get_open_row, game_progress
 
 # Constants for player representation
 EMPTY = 0
-AI_PLAYER = 1
-HUMAN_PLAYER = 2
-ROW_SIZE = 7
-COL_SIZE = 6
-
-from connectfour import get_open_row, player_turn
-
-
 
 def evaluate_position(board):
-    # Implement an evaluation function to assign a score to the board state.
-    # Consider factors like the number of pieces in a row, positional advantages, etc.
-    # Positive scores are good for the AI, and negative scores are good for the opponent.
-    # Return a higher score for better positions.
-
+    # Implement an evaluation function that assigns a score to the board state
     score = 0
 
     # Horizontal Evaluation
-
     for col in range(COL_SIZE - 3):
         for row in range(ROW_SIZE):
-
             horizontal_pieces = [board[row][col], board[row][col + 1], board[row][col + 2], board[row][col + 3]]
-            contains_AI = False
-            contains_Player = False
+            contains_AI = horizontal_pieces.count(AI_PLAYER)
+            contains_Player = horizontal_pieces.count(HUMAN_PLAYER)
 
-            for h in horizontal_pieces:
-                if h == HUMAN_PLAYER:
-                    contains_Player = True
-                if h == AI_PLAYER:
-                    contains_AI = True
-            
-            if contains_AI and not contains_Player:
-                # has at least 1 AI piece and no player pieces
+            if contains_AI > 0 and contains_Player == 0:
                 score += 1
-            elif contains_Player and not contains_AI:
-                # has at least 1 player piece and no AI pieces
+            elif contains_Player > 0 and contains_AI == 0:
                 score -= 1
             
     return score
 
-def game_is_over(board):
-    # Implement a function to check if the game is over (e.g., someone has won or it's a draw).
-    pass
+def minimax(state, game_board, depth, maximizing=True):
+    if depth == 0 or game_progress(game_board):
+        return evaluate_position(game_board)
 
-def get_legal_moves(board):
-    # Implement a function to return a list of legal moves (e.g., available columns to drop a piece).
-    pass
-
-def make_move(board, row, col, player):
-    # Implement a function to make a move on the board.
-    pass
-
-def undo_move(board, row, col):
-    # Implement a function to undo a move on the board.
-    pass
-
-ROW_SIZE = 7
-COL_SIZE = 6
-board_size = (COL_SIZE,ROW_SIZE) #size of the columns, rows
-cell_empty = 0 #empty cell on board
-player_x = 1 #player x piece on board
-player_y = 2 #player y piece on board
-game_over = False
-turn = 0
-
-#define game_board using 2D np array
-game_board = np.zeros(board_size, dtype= int)
+    if maximizing:
+        best_value = -float('inf')
+        for child in possible_states(state, game_board, AI_PLAYER):
+            value = minimax(state, child, depth - 1, False)
+            best_value = max(best_value, value)
+        return best_value
+    else:
+        best_value = float('inf')
+        for child in possible_states(state, game_board, HUMAN_PLAYER):
+            value = minimax(state, child, depth - 1, True)
+            best_value = min(best_value, value)
+        return best_value
 
 def possible_states(state, game_board, piece):
     new_states = []
-    for col in range(ROW_SIZE):
-        open_row = get_open_row(col)
-        game_board_copy = game_board
-        player_turn(game_board_copy, open_row, col, piece)
-        new_states.append(game_board_copy)
+    for col in range(COL_SIZE):
+        row = get_open_row(game_board, col)
+        if row is not None:
+            new_game_board = np.copy(game_board)
+            new_game_board[row][col] = piece
+            new_states.append(new_game_board)
     return new_states
-
-
-
-def minimax(state,game_board,game_over, depth, Maximizing=True):
-    """a function going through the tree and picking
-    the best possible move in a game. Also implementing AlphaBeta pruning"""
-    #Maybe setting a depth limit as well????
-
-    if game_over != True:
-
-        if depth == 0:
-            return evaluate_position(game_board)
-        
-        if Maximizing:
-            value = float('-inf')
-            next_states = possible_states(state, game_board)
-            for child in next_states:
-                value = max(value, minimax(child,game_board,game_over, depth - 1, False))
-            return value
-        else:
-            value = float('+inf')
-            next_states = possible_states(state, game_board)
-            for child in next_states:
-                value = min(value, minimax(child,game_board,game_over, depth - 1, True))
-            return value
-
-
-if __name__ == "__main__":
-    game_board[3][2] = 1
-    game_board[5][5] = 1
-    possible_states(6, game_board)
